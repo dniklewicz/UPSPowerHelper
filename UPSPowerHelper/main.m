a8b4721fc0993918b218d7486c526f557bc54257
Dariusz Niklewicz <dniklewicz@gmail.com>
2020-03-20 16:22:50 +0100

    Initial Commit

---
+++ UPSPowerHelper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UPSPowerHelper
+//
+//  Created by Dariusz Niklewicz on 20/03/2020.
+//  Copyright © 2020 Dariusz Niklewicz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
