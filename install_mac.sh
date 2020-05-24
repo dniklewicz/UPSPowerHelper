62cb6884d2212dd156aa004a1187822f20b7ad42
Dariusz Niklewicz <dniklewicz@gmail.com>
2020-05-25 00:30:25 +0200

    Updated install script

--- install_mac.sh
+++ install_mac.sh
@@ -5,27 +5,27 @@ PORT=${1:-58879}
 BINDIR=/usr/local/bin
 FILENAME=upspowerhelper.py
 
-XML="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\
-<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\
-<plist version=\"1.0\">\
-<dict>\
-    <key>Label</key>\
-    <string>com.dniklewicz.UPSPowerHelper</string>\
-    <key>ProgramArguments</key>\
-    <array>\
-        <string>/usr/bin/python</string>\
-        <string>$BINDIR/$FILENAME</string>\
-        <string>-p $PORT</string>\
-    </array>\
-    <key>StandardErrorPath</key>\
-    <string>$HOME/Library/Logs/UPSPowerHelper.log</string>\
-    <key>StandardOutPath</key>\
-    <string>$HOME/Library/Logs/UPSPowerHelper.log</string>\
-    <key>RunAtLoad</key>\
-    <true/>\
-    <key>KeepAlive</key>\
-    <true/>\
-</dict>\
+XML="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+    <key>Label</key>
+    <string>com.dniklewicz.UPSPowerHelper</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/usr/bin/python</string>
+        <string>$BINDIR/$FILENAME</string>
+        <string>-p $PORT</string>
+    </array>
+    <key>StandardErrorPath</key>
+    <string>$HOME/Library/Logs/UPSPowerHelper.log</string>
+    <key>StandardOutPath</key>
+    <string>$HOME/Library/Logs/UPSPowerHelper.log</string>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <true/>
+</dict>
 </plist>"
 
 LAUNCHITEM="$HOME/Library/LaunchAgents/com.dniklewicz.UPSPowerHelper.plist"
