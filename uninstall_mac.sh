b7bbc926a23cbe2dcee02f0b3fe6f1e4239f0b15
dniklewicz <dniklewicz@gmail.com>
2022-03-16 12:59:13 +0100

    Removing log file when uninstalling Power Server

--- uninstall_mac.sh
+++ uninstall_mac.sh
@@ -4,11 +4,13 @@ BINDIR=$HOME/.upspowerhelper
 FILENAME=upspowerhelper.py
 
 LAUNCHITEM="$HOME/Library/LaunchAgents/com.dniklewicz.UPSPowerHelper.plist"
+LOGFILE="$HOME/Library/Logs/UPSPowerHelper.log"
 
 echo "Stopping power server on port."
 
 rm -R "$BINDIR"
 launchctl unload -w "$LAUNCHITEM"
 rm "$LAUNCHITEM"
+rm "$LOGFILE"
 
 
