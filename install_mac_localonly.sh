e9132d29190cc7b2a562be6514a9dbfb542604a3
dniklewicz <dniklewicz@gmail.com>
2022-03-16 14:28:45 +0100

    Add explicit path to python3

--- install_mac_localonly.sh
+++ install_mac_localonly.sh
@@ -21,7 +21,7 @@ XML="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     <string>com.dniklewicz.UPSPowerHelper</string>
     <key>ProgramArguments</key>
     <array>
-        <string>python3</string>
+        <string>/usr/bin/python3</string>
         <string>$BINDIR/$FILENAME</string>
         <string>-p $PORT</string>
     </array>
