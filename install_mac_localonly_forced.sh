1ed67353a7adf520d7db35e261f737b209a2e721
Dariusz Niklewicz <d.niklewicz@kontakt.io>
2025-11-04 01:12:24 +0100

    Add variants with forced shutdown

---
+++ install_mac_localonly_forced.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+## Prevent the execution of the script if the user has root privileges
+if [[ $EUID -eq 0 ]]; then
+  echo "This script must NOT be run as root or sudo" 1>&2
+  exit 1
+fi
+
+PORT=${1:-58879}
+
+BINDIR=$HOME/.upspowerhelper
+FILENAME=upspowerhelper.py
+
+mkdir -p "$BINDIR"
+
+XML="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+    <key>Label</key>
+    <string>com.dniklewicz.UPSPowerHelper</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/usr/bin/python3</string>
+        <string>$BINDIR/$FILENAME</string>
+        <string>-p $PORT</string>
+    </array>
+    <key>StandardErrorPath</key>
+    <string>$HOME/Library/Logs/UPSPowerHelper-Error.log</string>
+    <key>StandardOutPath</key>
+    <string>$HOME/Library/Logs/UPSPowerHelper-Output.log</string>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <true/>
+</dict>
+</plist>"
+
+LAUNCHITEM="$HOME/Library/LaunchAgents/com.dniklewicz.UPSPowerHelper.plist"
+URL="https://raw.githubusercontent.com/dniklewicz/UPSPowerHelper/master/src/upspowerHelper_localonly_forced.py"
+
+# Creating ~/Library/LaunchAgents if needed
+LIB_LAUNCH_AGENTS="$HOME/Library/LaunchAgents/"
+
+if [ ! -d "$LIB_LAUNCH_AGENTS" ]
+then
+  echo "Creating directory $LIB_LAUNCH_AGENTS"
+  mkdir -p "$LIB_LAUNCH_AGENTS"
+else
+  echo "Directory $LIB_LAUNCH_AGENTS already exists"
+fi
+
+curl -L "$URL" --output "$FILENAME"
+mv "$FILENAME" "$BINDIR/$FILENAME"
+chmod +x "$BINDIR/$FILENAME"
+echo $XML > "$LAUNCHITEM"
+if (( `launchctl list | grep com.dniklewicz.UPSPowerHelper | wc -l` > 0 ))
+then
+  echo "Unloading previous instance of Power Server."
+  launchctl unload -w "$LAUNCHITEM"
+fi
+launchctl load -w "$LAUNCHITEM"
+# launchctl start "$LAUNCHITEM"
+
+echo "Starting forced power server on port $PORT"
---
+++ src/upspowerHelper_remote_forced.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import getopt
+import subprocess
+import sys
+
+def shutdown():
+    subprocess.call(['sudo', 'shutdown', '-h', 'now'])
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _set_response_json(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        if self.path.endswith('/info'):
+            self._set_response()
+            self.wfile.write(b'{"version": "2.3", "accessibility": "remote", "mode": "forced"}')
+
+    def do_POST(self):
+        if self.path.endswith('/shutdown'):
+            self._set_response_json()
+            self.wfile.write(b"OK")
+            shutdown()
+        elif self.path.endswith('/stop'):
+            self._set_response_json()
+            self.wfile.write(b"OK")
+            exit()
+
+server_port = 58879
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'p:')
+except getopt.GetoptError as err:
+    pass
+
+output = None
+verbose = False
+for o, a in opts:
+    if o == '-p':
+        server_port = int(a)
+
+server_address = ('0.0.0.0', server_port)
+httpd = HTTPServer(server_address, S)
+
+httpd.serve_forever()
